9be6690534058bcc344fed1da8e3486e3d026540
parin <parinchheda810@gmail.com>
2016-11-05 05:01:35 +0530

    added exceptions and tests for the same. Register mappings in c for all PORT/DDR registers

--- python_files/test_register.py
+++ python_files/test_register.py
@@ -3,16 +3,17 @@ from register import configreg
 
 
 class Testconfigreg(unittest.TestCase):
-    def test_configreg_raises_exceptions_invalid_pinnumber(self):
-        ''' exception of invalid pinnumber'''
+    def test_configreg_raises_exceptions(self):
         self.assertRaises(ValueError, configreg,
                           'DDRJ', Pins=[1, 2, 8], set_pins=True)
+        self.assertRaises(ValueError, configreg,
+                          'DDRZ', Pins=[1, 2, 8], set_pins=True)
 
     def test_config_data_buffer_value_for_pin(self):
         ''' pin numbers going from 0 to 7'''
         for i in range(0, 8):
             self.assertEqual(configreg('DDRJ', Pins=[i],
-                             set_pins=True), ['\t', chr(2**i), '\x01'])
+                             set_pins=True), ['\n', chr(2**i), '\x01'])
 
     def test_config_data_buffer_value_for_pincombinations(self):
         '''different combinations of pin numbers'''
@@ -23,7 +24,31 @@ class Testconfigreg(unittest.TestCase):
             pin_value = pin_value + 2**i
             self.assertEqual(configreg('DDRJ', Pins=pin_numbers,
                              set_pins=True),
-                             ['\t', chr(pin_value), '\x01'])
+                             ['\n', chr(pin_value), '\x01'])
+
+    def test_config_buffer_value_for_ports(self):
+        '''for Ports A-L'''
+        valid_port_names = [chr(i) for i in range(65, 77) if chr(i) != 'I']
+        pin_value = 8
+        pin_numbers = [3]
+        for j in valid_port_names:
+            port_name = 'DDR' + j
+            buffer_value_port = (chr(ord(port_name[3])-64))
+            self.assertEqual(configreg(port_name, Pins=pin_numbers,
+                             set_pins=True),
+                             [buffer_value_port, chr(pin_value), '\x01'])
+
+
+def suite():
+
+    suite = unittest.TestSuite()
+
+    suite.addTest(unittest.makeSuite(Testconfigreg))
+
+    return suite
+
 
 if __name__ == '__main__':
     unittest.main()
+    suiteFew = unittest.TestSuite()
+    unittest.TextTestRunner(verbosity=2).run(suite())
