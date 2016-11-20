352a54fb64f1a4e4a7ee9cbfa3ad09115ed5efcb
Saurav Shandilya <sauravs.iitb@gmail.com>
2016-11-20 07:11:55 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup #enables develop
+
+setup(name='robotapi',
+      version='1.0',
+      description='Python API for mobile robot control',
+      author='Parin Chheda, Saurav Shandilya',
+      author_email='sauravs.iitb@gmail.com',
+      license='BSD',
+      scripts=[],
+      url='https://github.com/sauravshandilya/SDES-2016-Project2',
+      packages=['source','tests'],
+      install_requires=[
+        "mock",
+        "serial",
+        "unittest",
+        "nose",
+      ],
+)
---
+++ source/__init__.py
@@ -0,0 +1,6 @@
+from .roboapi import *
+from .serial_connection import *
+
+
+del roboapi
+del serial_connection
